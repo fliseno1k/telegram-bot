08c819be5130d3e1fa733a22b28eda12fd2234b7
Aleksei Flis <fliseno1k@gmail.com>
2019-12-16 03:52:23 +1000

    fix228

--- bot.py
+++ bot.py
@@ -17,15 +17,15 @@ def upper(message: Message):
 @bot.message_handler(func=lambda message: True)
 def upper(message: Message):
     request = apiai.ApiAI('053380e328a047f79dc16bc3c739af1d').text_request() 
-    request.lang = 'ru'
-    request.session_id = 'BatlabAIBot'
-    request.query = message.text
+    request.lang = 'ru' 
+    request.session_id = 'Pretty' 
+    request.query = update.message.text
     responseJson = json.loads(request.getresponse().read().decode('utf-8'))
     response = responseJson['result']['fulfillment']['speech']
     if response:
-        bot.send_message(message.chat_id, response)
+        bot.send_message(chat_id=update.message.chat_id, text=response)
     else:
-        bot.send_message(message.chat_id, 'Я Вас не совсем понял!')
+        bot.send_message(chat_id=update.message.chat_id, text='Я Вас не совсем понял!')
 
       
 if __name__=="__main__":
